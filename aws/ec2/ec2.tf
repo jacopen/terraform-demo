2ea4f11b0d40900a883ec4b2efed1e254da1d5eb
Kazuto Kusama <jacopen@gmail.com>
2022-04-21 18:09:53 +0000

    Fix

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -14,9 +14,23 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_network_interface" "web" {
+  subnet_id   = data.aws_subnet.public_0
+  # private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.web.id
+    device_index         = 0
+  }
 
   tags = {
     Name = "HelloWorld"
