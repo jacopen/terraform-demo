ada29b9a8653e24f1de13a51c2fbfd875043d6c6
Kazuto Kusama <jacopen@gmail.com>
2022-04-22 01:35:04 +0000

    Add VMs

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -1,3 +1,4 @@
+//// Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -11,12 +12,12 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] 
 }
 
+//// NIC
 resource "aws_network_interface" "web" {
   subnet_id   = data.aws_subnet.public_0.id
-  # private_ips = ["172.16.10.100"]
 
   tags = {
     Name = "primary_network_interface"
@@ -34,5 +35,69 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+
+//// NIC
+resource "aws_network_interface" "web2" {
+  subnet_id   = data.aws_subnet.public_0.id
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.web2.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "No2"
+  }
+}
+
+resource "aws_network_interface" "instance" {
+  for_each = toset(["Web1", "Web2", "Web3"])
+  subnet_id   = data.aws_subnet.public_0.id
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each = toset(["Web1", "Web2", "Web3"])
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.small"
+
+  network_interface {
+    network_interface_id = aws_network_interface.instance[each.key].id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = each.key
+  }
+}
+
+
+resource "aws_instance" "test" {
+  ami           = "ami-088da9557aae42f39"
+  instance_type = "t3.micro"
+
+  # network_interface {
+  #   network_interface_id = aws_network_interface.web.id
+  #   device_index         = 0
+  # }
+
+  tags = {
+    Name = "test",
+    Created = "terraform"
   }
 }
